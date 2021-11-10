a5ce8fb492a8c075865148acf5b16f6eaea50689
James Spencer <jameskcs1254@gmail.com>
2021-11-10 16:17:16 +0000

    Add display component. Add state. Import uniqid.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
+import Display from './components/Display';
+import uniqid from 'uniqid'
 
 class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputs: {
+        general: {
+          name:{
+            value:'',
+            id: uniqid()
+          } 
+        }
+      },
+      cvData: {
+        general:{},
+        education:{},
+        
+
+      }
+    }
+  }
   render(){
     return (
       <div className="App">
-        Hello?
+        <Display name="James S" location="London" mobile="000000000" email="blaah@gmail.com" tagline="Does this work?" profile="Will this app work please" />
       </div>
     );
   }
