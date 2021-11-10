9f67d6b48cd9dfcad3bcef4d2aed51f93aa86d86
James Spencer <jameskcs1254@gmail.com>
2021-11-10 17:43:19 +0000

    Add class boilerplate

---
+++ src/components/input.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+
+class Input extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="Input">
+
+      </div>
+    )
+  }
+}
+
+export default Input
