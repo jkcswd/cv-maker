05eeb4a3c8361dfcf60676f4d9c389001564b7e7
James Spencer <jameskcs1254@gmail.com>
2021-11-11 16:28:55 +0000

    remove useles constructor

--- src/components/InputDate.js
+++ src/components/InputDate.js
@@ -1,10 +1,6 @@
 import React, {Component} from 'react'
 
 class InputDate extends Component {
-  constructor(props){
-    super(props);
-  }
-
   render() {
     return (
       <div className="Input">
