92ecae276701f6e7cfa7ac022e9fb8dca8e7495e
James Spencer <jameskcs1254@gmail.com>
2021-11-11 16:21:21 +0000

    add InputShort component calls

--- src/components/input.js
+++ src/components/input.js
@@ -8,14 +8,33 @@ class Input extends Component {
   render() {
     return (
       <div className="Input">
-        <InputDate handleChange={this.props.handleChange} handleSubmit={this.props.handleSubmit}/>
-        <InputLong handleChange={this.props.handleChange} handleSubmit={this.props.handleSubmit}/>
         <InputShort 
           handleChange={this.props.handleChange} 
           handleSubmit={this.props.handleSubmit} 
           inputData={this.props.inputData}  
           propertyCategory="general" 
           propertyValue="name"
+        />
+        <InputShort 
+          handleChange={this.props.handleChange} 
+          handleSubmit={this.props.handleSubmit} 
+          inputData={this.props.inputData}  
+          propertyCategory="general" 
+          propertyValue="location"
+        />
+        <InputShort 
+          handleChange={this.props.handleChange} 
+          handleSubmit={this.props.handleSubmit} 
+          inputData={this.props.inputData}  
+          propertyCategory="general" 
+          propertyValue="mobile"
+        />
+        <InputShort 
+          handleChange={this.props.handleChange} 
+          handleSubmit={this.props.handleSubmit} 
+          inputData={this.props.inputData}  
+          propertyCategory="general" 
+          propertyValue="email"
         />
       </div>
     )
