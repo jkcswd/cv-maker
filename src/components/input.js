1544211210e0dc0328852e5cf5fbf661a18c005a
James Spencer <jameskcs1254@gmail.com>
2021-11-11 13:13:30 +0000

    Import and call child components

--- src/components/input.js
+++ src/components/input.js
@@ -1,14 +1,27 @@
 import React, {Component} from 'react'
 
+import InputDate from './inputDate';
+import InputLong from './inputLong';
+import InputShort from './inputShort';
+
 class Input extends Component {
   constructor(props){
     super(props);
+
   }
 
   render() {
     return (
       <div className="Input">
-
+        <InputDate handleChange={this.props.handleChange} handleSubmit={this.props.handleSubmit}/>
+        <InputLong handleChange={this.props.handleChange} handleSubmit={this.props.handleSubmit}/>
+        <InputShort 
+          handleChange={this.props.handleChange} 
+          handleSubmit={this.props.handleSubmit} 
+          inputData={this.props.inputData}  
+          propertyCategory="general" 
+          propertyValue="name"
+        />
       </div>
     )
   }
