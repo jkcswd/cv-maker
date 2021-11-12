4d3b73242e543478649a9c2373b55dbed49e05f3
James Spencer <jameskcs1254@gmail.com>
2021-11-12 18:22:09 +0000

    Create InputWork component off InputEducation

---
+++ src/components/InputWork.js
@@ -0,0 +1,64 @@
+import React, {Component} from 'react'
+
+class InputWork extends Component {
+  render() {
+    return (
+      <div className="InputWork">
+        <form>
+          <label>
+            Company Name:
+            <input 
+              type="text" 
+              data-category="experience"
+              data-value="name"
+              value={this.props.inputData.experience.name.value} 
+              onChange={this.props.handleChange}
+            />
+          </label>
+          <label>
+            Job Title:
+            <input 
+              type="text" 
+              data-category="experience"
+              data-value="position"
+              value={this.props.inputData.experience.position.value}
+              onChange={this.props.handleChange}
+            />
+          </label>
+          <label>
+            Date from:
+            <input 
+              data-category="experience"
+              data-value="date"
+              type="date" 
+              value={this.props.inputData.experience.date.from.value}
+              onChange={this.props.handleChange}
+            />
+          </label>
+          <label>
+            Date to:
+            <input 
+              data-category="experience"
+              data-value="date"
+              type="date" 
+              value={this.props.inputData.experience.date.from.value}
+              onChange={this.props.handleChange}
+            />
+          </label>
+          <label>
+            Job Description:
+            <textarea 
+              data-category="experience"
+              data-value="jobTasks"
+              value={this.props.inputData.experience.jobTasks.value}
+              onChange={this.props.handleChange}
+            />
+          </label>
+          <input type="submit" value="Add" />
+        </form>
+      </div>
+    )
+  }
+}
+
+export default InputWork
